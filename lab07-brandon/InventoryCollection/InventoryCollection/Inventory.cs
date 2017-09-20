4bf58c9ffbc0a81c22f6f55da863ff673eb97c91
Brandon Little <brlittle86@gmail.com>
2017-09-20 10:44:48 -0700

    filled out Inventory enum and Product class

---
+++ lab07-brandon/InventoryCollection/InventoryCollection/Inventory.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryCollection
+{
+    class Inventory<Product> : IEnumerable<Product>
+    {
+        Product[] products = new Product[10];
+        int count = 0;
+
+        public void Add(Product item)
+        {
+            Product[] temp = new Product[(count + 1)];
+            int tempCount = 0;
+            foreach (Product product in products)
+            {
+                temp[tempCount] = product;
+                tempCount++;
+            }
+            products = temp;
+            products[count] = item;
+            count++;
+        }
+
+        public void Remove(Product item)
+        {
+            Product[] temporary = new Product[(count - 1)];
+            int tempCount = 0;
+            foreach (Product product in products)
+            {
+                if (!product.Equals(item))
+                {
+                    temporary[tempCount] = product;
+                    tempCount++;
+                }
+            }
+            products = temporary;
+            count--;
+        }
+
+        public void View()
+        {
+            foreach (Product item in products)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public IEnumerator<Product> GetEnumerator()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return products[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
